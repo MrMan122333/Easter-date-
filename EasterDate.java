804a23fdc4308048472e6654fd309ed032c4416b
MrMan122333 <106705717+MrMan122333@users.noreply.github.com>
2025-09-04 15:05:23 +0000

    hi

--- EasterDate.java
+++ EasterDate.java
@@ -1,5 +1,34 @@
+//Daniel Tiso
+//This project caculates the date that easter will land on
 public class EasterDate{
  public static void main(String[] args) {
+   int y=2025;
+   int a=y%19;
+   int b=y/100;
+   int c=y%100;
+   int d=b/4; 
+   int e=b%4;
+   int f=(b+8)/25;
+   int g=(b-f+1)/3;
+   int h=((19*a)+b-d-g+15)%30;
+   int i=c%4;
+   int k=c/4;
+   int 
+   int a=8;
+    int b=20;
+    int c=3;
+    int d=5;
+    int e=0;
+    int f=1;
+    int g=6;
+    int h=26;
+    int i=0;
+    int k=3;
+    int r=3;
+    int m=0;
+    int n=9;
+    int p=3; 
+    int y=2025;
     
  }
 }
